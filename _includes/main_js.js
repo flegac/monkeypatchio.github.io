57399f6791979ae4ac3d525d1dafc2c22810a6f1
Emmanuel Vinas <emmanuel.vinas@ingenico.com>
2015-10-14 14:52:27 +0200

    separate main js

---
+++ _includes/main_js.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/bootstrap/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/bootstrap-hover-dropdown.min.js"></script>
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/back-to-top.js"></script>             
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/jquery-placeholder/jquery.placeholder.js"></script>                                                                  
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/jquery-match-height/jquery.matchHeight-min.js"></script>
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/FitVids/jquery.fitvids.js"></script>
+<script type="text/javascript" src="{{ site.baseurl }}/public/js/main.js"></script>
