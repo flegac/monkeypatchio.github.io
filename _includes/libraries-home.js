239d611e19aaf213bd47a91dd89569ba87e6ea47
evinas <vinas.emmanuel@gmail.com>
2015-10-28 08:29:14 +1100

    fixed index

---
+++ _includes/libraries-home.js
@@ -0,0 +1,7 @@
+<!-- Flexslider -->    
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/flexslider/jquery.flexslider-min.js"></script> 
+<script type="text/javascript" src="{{ site.baseurl }}/public/js/flexslider-custom.js"></script>     
+
+<!-- Owl Carousel -->
+<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/owl-carousel/owl.carousel.js"></script> 
+<script type="text/javascript" src="{{ site.baseurl }}/public/js/owl-custom.js"></script> 
