962bdbc364a336543d4e630a90c83c88bbb7eed4
Emmanuel Vinas <emmanuel.vinas@ingenico.com>
2015-10-30 14:52:51 +0100

    update jobs with accordion

---
+++ _includes/js-jobs.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+	$(document).ready(function() {
+	    $('#accordion').on('show.bs.collapse', function () {
+			$('#accordion .in').collapse('hide');
+		});	
+		function toggleChevron(e) {
+			$(e.target)
+				.prev('.row')
+				.find("i.indicator")
+				.toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+		}
+		$('#accordion').on('hidden.bs.collapse', toggleChevron);
+		$('#accordion').on('shown.bs.collapse', toggleChevron);
+	}
+</script>
