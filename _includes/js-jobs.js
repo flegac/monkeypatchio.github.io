d49d627e0f6eba981204f3060d67ec259e77f009
evinas <vinas.emmanuel@gmail.com>
2015-11-01 00:53:40 +1100

    updated jobs page

--- _includes/js-jobs.js
+++ _includes/js-jobs.js
@@ -6,10 +6,10 @@
 		function toggleChevron(e) {
 			$(e.target)
 				.prev('.item-inner')
-				.find("p.indicator")
+				.find('i.indicator')
 				.toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
 		}
 		$('#accordion').on('hidden.bs.collapse', toggleChevron);
 		$('#accordion').on('shown.bs.collapse', toggleChevron);
-	}
+	});
 </script>
