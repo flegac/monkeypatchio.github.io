d7362525327ab7605df0056f8d750c43ea1c29cb
evinas <vinas.emmanuel@gmail.com>
2015-11-01 06:36:27 +1100

    update contact

--- _includes/js-jobs.js
+++ _includes/js-jobs.js
@@ -11,5 +11,16 @@
 		}
 		$('#accordion').on('hidden.bs.collapse', toggleChevron);
 		$('#accordion').on('shown.bs.collapse', toggleChevron);
+
+
+	$('#accordion').on('shown.bs.collapse', function (e) {
+        var offset = $('.panel.panel-default > .panel-collapse.in').offset();
+        if(offset) {
+            $('html,body').animate({
+                scrollTop: $('.panel-title a').offset().top -20
+            }, 500); 
+        }
+    }); 
+
 	});
 </script>
