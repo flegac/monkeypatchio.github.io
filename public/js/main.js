0fd880117d0a62096db37b846bd4fe55eb744d59
Emmanuel Vinas <emmanuel.vinas@ingenico.com>
2015-11-02 10:29:46 +0100

    removed usused js

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,5 @@
 $(document).ready(function() {
 
-    /* ======= Twitter Bootstrap hover dropdown ======= */   
-    /* Ref: https://github.com/CWSpear/bootstrap-hover-dropdown */ 
-    /* apply dropdownHover to all elements with the data-hover="dropdown" attribute */
-    
-    $('[data-hover="dropdown"]').dropdownHover();
-    
-    /* ======= jQuery Responsive equal heights plugin ======= */
-    /* Ref: https://github.com/liabru/jquery-match-height */
-    
-     $('#who .item-inner').matchHeight();    
-     $('#testimonials .item-inner .quote').matchHeight(); 
-     $('#latest-blog .item-inner').matchHeight(); 
-     $('#services .item-inner').matchHeight();
-     $('#team .item-inner').matchHeight();
-     $('#careers .item-inner').matchHeight();
-     $('#offers .item-inner').matchHeight();
-     
-    /* ======= jQuery Placeholder ======= */
-    /* Ref: https://github.com/mathiasbynens/jquery-placeholder */
-    
-    $('input, textarea').placeholder();         
-    
-    /* ======= jQuery FitVids - Responsive Video ======= */
-    /* Ref: https://github.com/davatron5000/FitVids.js/blob/master/README.md */    
-    $(".video-container").fitVids();   
-    
-	
-	/* ======= Fixed Header animation ======= */ 
-        
-    $(window).on('scroll', function() {
-         
-         if ($(window).scrollTop() > 80 ) {
-             $('#header').addClass('header-shrink');
-         }
-         else {
-             $('#header').removeClass('header-shrink');             
-         }
-    }); 
-
     /* I18N management */
 
     $.i18n.init({
@@ -67,4 +28,37 @@ $(document).ready(function() {
           $('body').i18n();
         });
     });
+
+    /* ======= Twitter Bootstrap hover dropdown ======= */   
+    /* Ref: https://github.com/CWSpear/bootstrap-hover-dropdown */ 
+    /* apply dropdownHover to all elements with the data-hover="dropdown" attribute 
+    $('[data-hover="dropdown"]').dropdownHover();*/
+    
+    /* ======= jQuery Responsive equal heights plugin ======= */
+    /* Ref: https://github.com/liabru/jquery-match-height */
+    
+     $('#services .item-inner').matchHeight();
+     $('#who .item-inner').matchHeight();    
+     $('#team .item-inner').matchHeight();
+     $('#careers .item-inner').matchHeight();
+     $('#offers .item-inner').matchHeight();
+     
+    /* ======= jQuery Placeholder ======= */
+    /* Ref: https://github.com/mathiasbynens/jquery-placeholder */
+    
+    $('input, textarea').placeholder();         
+    
+	/* ======= Fixed Header animation ======= */ 
+        
+    $(window).on('scroll', function() {
+         
+         if ($(window).scrollTop() > 80 ) {
+             $('#header').addClass('header-shrink');
+         }
+         else {
+             $('#header').removeClass('header-shrink');             
+         }
+    }); 
+
+    
 });
