4dc0d0cadc94701f47b4e78f6bfd523af1354d27
Emmanuel Vinas <emmanuel.vinas@ingenico.com>
2015-11-02 13:03:44 +0100

    removed unused js

--- _includes/js-jobs.js
+++ _includes/js-jobs.js
@@ -6,17 +6,6 @@
 		});	
 
 		/** Animate to current job */
-		function toggleChevron(e) {
-			$(e.target)
-				.prev('.item-inner')
-				.find('i.indicator')
-				.toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
-		}
-		$('#accordion').on('hidden.bs.collapse', toggleChevron);
-		$('#accordion').on('shown.bs.collapse', toggleChevron);
-
-
-		/** Animate to current job */
     	$('#accordion').on('shown.bs.collapse', function (e) {
         	var offset = $('.panel.panel-default > .panel-collapse.in').offset();
         	if(offset) {
