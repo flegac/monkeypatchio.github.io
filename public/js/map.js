475dc0762ae265fe824e3ce768f1e9ebade4282f
Emmanuel Vinas <emmanuel.vinas@ingenico.com>
2016-01-20 10:20:02 +0100

    Updated address

--- public/js/map.js
+++ public/js/map.js
@@ -3,15 +3,15 @@ jQuery(document).ready(function(){
 
     map = new GMaps({
         div: '#map',
-        lat: 43.5378893,
-        lng: 1.5329330,
+        lat: 43.5518078,
+        lng: 1.4895341,
     });
     map.addMarker({
-        lat: 43.5378893,
-        lng: 1.5329330,
+        lat: 43.5518078,
+        lng: 1.4895341,
         title: 'Address',      
         infoWindow: {
-            content: '<h5 class="title">MonkeyPatch</h5><p><span class="region">7 ter chemin du Tricou</span><br><span class="postal-code">31670</span><br><span class="country-name">France</span></p>'
+            content: '<h5 class="title">MonkeyPatch</h5><p><span class="region">12 avenue de l\'Europe, 31650 Ramonville-Saint-Agne</span><br><span class="postal-code">31670</span><br><span class="country-name">France</span></p>'
         }
         
     });
