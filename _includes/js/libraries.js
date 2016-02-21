d2eaf88addec96cca03546ffa9f287e7a4828d0c
evinas <vinas.emmanuel@gmail.com>
2016-02-22 02:11:55 +1100

    Updated libs

--- _includes/js/libraries.js
+++ _includes/js/libraries.js
@@ -2,8 +2,8 @@
 <script type="text/javascript" src="{{ site.baseurl }}/public/plugins/bootstrap/js/bootstrap.min.js"></script> 
 <script type="text/javascript" src="{{ site.baseurl }}/public/plugins/bootstrap-hover-dropdown.min.js"></script>
 <script type="text/javascript" src="{{ site.baseurl }}/public/plugins/back-to-top.js"></script>             
-<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/jquery-placeholder/jquery.placeholder.js"></script>                                                                  
-<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/jquery-match-height/jquery.matchHeight-min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-placeholder/2.3.1/jquery.placeholder.js"></script>      
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.0/jquery.matchHeight-min.js"></script>
 <script type="text/javascript" src="{{ site.baseurl }}/public/plugins/FitVids/jquery.fitvids.js"></script>
 <!-- Jquery cookie -->   
 <script type="text/javascript" src="{{ site.baseurl }}/public/js/jquery.cookie.js"></script>
