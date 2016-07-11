d94d2236188bf97f6a91b2048720366f9eef0df8
Igor Laborie <igor@monkeypatch.io>
2016-07-11 18:41:42 +0200

    Update site at lundi 11 juillet 2016, 18:41:42 (UTC+0200)

--- _includes/js/libraries-form.js
+++
@@ -1,7 +0,0 @@
-<!-- Form Validation -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script> 
-<script type="text/javascript" src="{{ site.baseurl }}/public/js/form-validation-custom.js"></script>
-
-<!-- Form iOS fix -->
-<script type="text/javascript" src="{{ site.baseurl }}/public/plugins/isMobile/isMobile.min.js"></script>
-<script type="text/javascript" src="{{ site.baseurl }}/public/js/form-mobile-fix.js"></script>
--- _includes/js/libraries-maps.js
+++
@@ -1,4 +0,0 @@
-<!-- Google Map -->
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script> 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.22/gmaps.min.js"></script> 
-<script type="text/javascript" src="{{ site.baseurl }}/public/js/map.js"></script>
