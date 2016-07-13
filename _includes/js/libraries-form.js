a028811fb0ddedfc4e0ad6f45d789f48d8cf4cf4
Igor Laborie <igor@monkeypatch.io>
2016-07-13 14:41:21 +0200

    Header

--- _includes/js/libraries-form.js
+++
@@ -1,7 +0,0 @@
-<!-- Form Validation -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script> 
-<script type="text/javascript" src="{{ site.baseurl_root }}/public/js/form-validation-custom.js"></script>
-
-<!-- Form iOS fix -->
-<script type="text/javascript" src="{{ site.baseurl_root }}/public/plugins/isMobile/isMobile.min.js"></script>
-<script type="text/javascript" src="{{ site.baseurl_root }}/public/js/form-mobile-fix.js"></script>
--- _includes/js/libraries.js
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script> 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-hover-dropdown/2.2.1/bootstrap-hover-dropdown.min.js"></script>
-<script type="text/javascript" src="{{ site.baseurl_root }}/public/plugins/back-to-top.js"></script>             
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.0/jquery.matchHeight-min.js"></script>
